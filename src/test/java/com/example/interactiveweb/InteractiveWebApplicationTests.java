549d4df062bcbf6d3bf16b4c9213fd794e223e0e
r-uwu <yarrow.uwu@gmail.com>
2026-05-12 17:34:28 +0900

    프로젝트 생성

---
+++ src/test/java/com/example/interactiveweb/InteractiveWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interactiveweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractiveWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
